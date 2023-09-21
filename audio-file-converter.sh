532aace082557864094488fa10ebf6613e0a6fd3
David Ferlay <davidferlay.life@gmail.com>
2023-09-21 18:45:16 +0200

    Update

--- audio-file-converter.sh
+++ audio-file-converter.sh
@@ -4,7 +4,23 @@
 output_bit_depth=24
 output_sample_rate=44100
 
-# Function to check the sample rate of an audio file using soxi
+convert_to_kHz() {
+  local sample_rate="$1"
+  echo "$((sample_rate / 1000)).$((sample_rate % 1000 / 100)) kHz"
+}
+
+# Define the supported extensions using the output of the 'sox' command
+get_supported_extensions() {
+  supported_extensions=$(sox --help | grep -A1 "^AUDIO FILE FORMATS:" | head -1 | sed 's/AUDIO FILE FORMATS: //g' | tr -s ' ' '\n')
+  # Make sure it returned somes values
+  if [ -z "$supported_extensions" ]; then
+  	echo "Error: No supported extensions found using sox command."
+  	exit 1
+  else echo "Supported extensions: $(echo $supported_extensions | tr -s '\n' ' ')"
+  fi
+}
+
+# Function to check the sample rate of an audio file using 'soxi'
 get_sample_rate() {
   local input_file="$1"
   soxi -r "$input_file"
@@ -21,16 +37,17 @@ convert_audio() {
   local output_file="${input_file}"
 
   # Check the sample rate of the input file
-  local sample_rate=$(get_sample_rate "$input_file")
+  local input_sample_rate=$(get_sample_rate "$input_file")
 
   # If the sample rate does not match the one defined, perform the conversion
-  if [ -n "$sample_rate" ] && [ "$sample_rate" != "$output_sample_rate" ]; then
-    echo "- $input_file: Converting from $sample_rate Hz to $output_sample_rate Hz"
+  if [ -n "$input_sample_rate" ] && [ "$input_sample_rate" != "$output_sample_rate" ]; then
+    echo "- Converting from $(convert_to_kHz "$input_sample_rate") to $(convert_to_kHz "$output_sample_rate"): $input_file"
+    # The magic happens here
     sox "$input_file" -b $output_bit_depth -r $output_sample_rate "$temp_file" && mv "$temp_file" "$output_file"
-  elif [ "$sample_rate" == "$output_sample_rate" ]; then
-    echo "- $input_file: Already in $output_sample_rate Hz. Skipping"
+  elif [ "$input_sample_rate" == "$output_sample_rate" ]; then
+    echo "- Already in $(convert_to_kHz "$output_sample_rate"): $input_file - Skipping"
   else
-    echo "- Error: Unable to determine sample rate for $input_file. Skipping."
+    echo "- Warning: Unable to determine sample rate for $input_file. Skipping."
   fi
 }
 
@@ -38,12 +55,25 @@ convert_audio() {
 echo "Starting script..."
 echo
 sox --version
+get_supported_extensions
 echo
-find . -type f \( -iname "*.wav" -o -iname "*.aif" \) | while read -r audio_file; do
-  convert_audio "$audio_file"
+
+# List files recursively from the current directory
+find . -type f | while read -r file; do
+  # Get the file extension of the current file
+  file_extension=$(echo "$file" | awk -F. '{print $NF}')
+  
+  # Check if the file extension matches one of the supported extensions
+  if echo "$supported_extensions" | grep -qw "$file_extension"; then
+  	# If yes, convert file
+    convert_audio "$file"
+  else
+    echo "- Unsupported file extension: $file"
+  fi
 done
+
 echo
 echo "Conversion completed."
 
-# Remove the following line if you don't want the script to sleep indefinitely
+# Prevent shell windo to close, to check logs
 sleep infinity
